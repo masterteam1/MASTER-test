9462182067b4706a078910b77589c6f697686161
masterteam1 <zozo.prince7@gmail.com>
2016-09-23 21:56:43 +0300

    master_team

--- plugins/nospam.lua
+++ plugins/nospam.lua
@@ -1,7 +1,7 @@
 do 
 function run(msg, matches) 
 if msg.media and not is_momod(msg)  then 
-return "ممنوع ارسال الصور في هذا المجموعة يا "\n\n".."USERNAME @"..msg.from.username.."\n".."⚠NAME "..msg.from.first_name 
+return "ممنوع ارسال الصور في هذا المجموعة يا \n\n".."USERNAME @"..msg.from.username.."\n".."⚠NAME "..msg.from.first_name 
 else 
 return msg
 end 
