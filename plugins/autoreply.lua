6d1d4ffa24c3c18247c1b7f6286576fcf6946abc
masterteam1 <zozo.prince7@gmail.com>
2016-09-24 22:29:01 +0300

    master_team

--- plugins/autoreply.lua
+++ plugins/autoreply.lua
@@ -9,7 +9,7 @@ do
 local function master(msg, matches) 
 if ( msg.text ) then
   if ( msg.to.type == "user" ) then
-     return "اهلاً في الرد الألي لبوت MASTER\n لتحدث مع مبرمج البوت ادخل المعرف @Mortadha1997\nللدخول الى مجموعة الدعم:\nhttps://telegram.me/joinchat/Ay1Ox0ErjRMiPi3YbpJ6rw"
+     return "اهلاً في الرد الألي لبوت MASTER\n لتحدث مع مبرمج البوت ادخل المعرف @Mortadha1997\nللدخول الى مجموعة الدعم:\nhttps://telegram.me/joinchat/Ay1Ox0ErjRMiPi3YbpJ6rw\nاول ارسل:\n/join master su\nقناة السورس:\n@Master_CH"
     end 
   end 
 end 
